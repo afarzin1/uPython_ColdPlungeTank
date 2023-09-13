b9a247790c352c82a9c5ba45d5e83bfb03757a20
Arya Farzin <afarzin@tesla.com>
2023-09-13 16:20:27 -0700

    updated icepack algorithm

--- main.py
+++ main.py
@@ -183,10 +183,10 @@ def get_current_ambient_temperature(api_key, lat, lon):
         return f"Error: {response.status_code}"
 
 def calculate_ice_packs(T_initial, T_final):
-    m_water = 184955.685  # mass of 48.85 gallons of water in grams
-    c = 4.18  # specific heat capacity of water in J/g°C
-    Lf = 334  # heat of fusion for water in J/g
-    m_ice_pack = 520  # mass of one ice pack in grams
+    m_water = 184955  # mass of 48.85 gallons of water in grams
+    c = 7.18  # specific heat capacity of water in J/g°C
+    Lf = 300  # heat of fusion for water in J/g
+    m_ice_pack = 2600  # mass of one ice pack in grams
 
     E_needed = m_water * c * (T_initial - T_final)
     E_ice_pack = m_ice_pack * (Lf + c * T_final)
